3eaa2c9c59b89836d58c976b2dc2029a0acd510e
Jimmaphy <wschaijk@outlook.com>
2025-10-06 14:54:15 +0200

    Added creation of Docker images

--- install.sh
+++ install.sh
@@ -3,6 +3,21 @@
 # Load environment variables from the .env file
 source .env
 
+# Check whether git and docker are installed
+for package in "${REQUIRED_PACKAGES[@]}"; do
+    if ! command -v $pkg &> /dev/null
+    then
+        echo "$pkg could not be found, please install $pkg first."
+        exit
+    fi
+done
+
 # Clone the repositories using the URLs from the .env file
-git clone $FRONTEND_GITHUB_URL $FRONTEND_DIR
-git clone $BACKEND_GITHUB_URL $BACKEND_DIR
+printf "\nCloning repositories..."
+git clone $FRONTEND_GITHUB_URL $FRONTEND_DIR -b $FRONTEND_CURRENT_BRANCH
+git clone $BACKEND_GITHUB_URL $BACKEND_DIR -b $BACKEND_CURRENT_BRANCH
+
+# Create the docker containers using docker build
+printf "\nBuilding Docker images..."
+docker build -t $FRONTEND_IMAGE_NAME -f $FRONTEND_DIR/$FRONTEND_DOCKERFILE_NAME $FRONTEND_DIR
+docker build -t $BACKEND_IMAGE_NAME -f $BACKEND_DIR/$BACKEND_DOCKERFILE_NAME $BACKEND_DIR
--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,15 @@
 # Load all environment variables from the .env file
 source .env
 
+# Remove the docker images using the image names from the .env file
+printf "\nRemoving Docker images..."
+docker rmi $FRONTEND_IMAGE_NAME
+docker rmi $BACKEND_IMAGE_NAME
+
 # Remove the cloned repositories using the directory names from the .env file
-echo "Removing cloned repositories..."
+printf "\nRemoving cloned repositories..."
 rm -rf $FRONTEND_DIR
 rm -rf $BACKEND_DIR
+
+# Print a completion message
+printf "\nUninstallation complete!\n"
