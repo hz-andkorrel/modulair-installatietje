ec5da4bbe2fa5eba44bff36d0f7ec8c170e051b6
Jimmaphy <wschaijk@outlook.com>
2025-10-06 13:34:56 +0200

    Downloading and removing of repositories

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Hello World"
+# Load environment variables from the .env file
+source .env
+
+# Clone the repositories using the URLs from the .env file
+git clone $FRONTEND_GITHUB_URL $FRONTEND_DIR
+git clone $BACKEND_GITHUB_URL $BACKEND_DIR
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "hello world"
+# Load all environment variables from the .env file
+source .env
+
+# Remove the cloned repositories using the directory names from the .env file
+echo "Removing cloned repositories..."
+rm -rf $FRONTEND_DIR
+rm -rf $BACKEND_DIR
